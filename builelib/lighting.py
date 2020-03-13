6608bb892197a84a9be0063aaaa95d3cfbc06405
Masato Miyata <myt.masato@gmail.com>
2020-03-13 16:53:15 +0900

    照明の計算プログラムの作成

--- builelib/lighting.py
+++ builelib/lighting.py
@@ -2,43 +2,6 @@
 import json
 import pprint as pp
 
-#%%
-
-# テンプレートjsonの読み込み
-with open('../../inputdata.json', 'r') as f:
-    inputdata = json.load(f)
-
-# データベースjsonの読み込み
-with open('../database/RoomUsageSchedule.json', 'r') as f:
-    RoomUsageSchedule = json.load(f)
-with open('../database/lightingControl.json', 'r') as f:
-    lightingCtrl = json.load(f)
-with open('../database/ROOM_STANDARDVALUE.json', 'r') as f:
-    RoomStandardValue = json.load(f)
-
-# 室の情報を取得する関数
-def get_roomSpec(floorName,roomName,roomsdata):
-    for room in roomsdata:
-        if room["floorName"] == floorName and room["roomName"] == roomName:
-            buildingType = room["buildingType"]
-            roomType = room["roomType"]
-            roomArea = room["roomArea"]
-            break
-    
-    return (buildingType,roomType,roomArea)
-
-# 電気の量1キロワット時を熱量に換算する係数
-fprime = 9760
-
-# 計算結果モデル
-resultJson = {
-    "E_lighting": None,
-    "Es_lighting": None,
-    "BEI_L": None,
-    "lighting":[
-    ]
-}
-
 # 室の形状に応じて定められる係数（仕様書4.4）
 def set_roomIndexCoeff(roomIndex):
 
@@ -62,86 +25,142 @@ def set_roomIndexCoeff(roomIndex):
     
     return roomIndexCoeff
 
-# %%
+# 室の情報を取得する関数
+def get_roomSpec(floorName,roomName,roomsdata):
+    for room in roomsdata:
+        if room["floorName"] == floorName and room["roomName"] == roomName:
+            buildingType = room["buildingType"]
+            roomType = room["roomType"]
+            roomArea = room["roomArea"]
+            break
+    
+    return (buildingType,roomType,roomArea)
 
-# 室毎（照明系統毎）にループ
-E_lighting = 0    # 設計一次エネルギー消費量 [GJ]
-Es_lighting = 0   # 基準一次エネルギー消費量 [GJ]
 
-for isys in inputdata["LightingSystems"]:
 
-    # 建物用途、室用途、室面積の取得
-    (buildingType, roomType, roomArea) = get_roomSpec(isys["floorName"],isys["roomName"],inputdata["Rooms"])
+#%%
+def lighting(inputdata):
 
-    # 基準一次エネルギー消費量 [MJ]
-    Es_room = RoomStandardValue[buildingType][roomType]["照明"] * roomArea
-    Es_lighting += Es_room  # 出力用に積算
+    # データベースjsonの読み込み
+    with open('./builelib/database/RoomUsageSchedule.json', 'r') as f:
+        RoomUsageSchedule = json.load(f)
+    with open('./builelib/database/lightingControl.json', 'r') as f:
+        lightingCtrl = json.load(f)
+    with open('./builelib/database/ROOM_STANDARDVALUE.json', 'r') as f:
+        RoomStandardValue = json.load(f)
 
-    # 年間照明点灯時間 [時間]
-    opeTime = RoomUsageSchedule[buildingType][roomType]["年間照明点灯時間"]
+    # 電気の量1キロワット時を熱量に換算する係数
+    fprime = 9760
 
-    ## 室の形状に応じて定められる係数（仕様書4.4）
-    # 室指数
-    if isys["roomIndex"] != None:
-        roomIndex = isys["roomIndex"]
-    elif isys["roomWidth"] != None and isys["roomDepth"] != None and isys["unitHeight"] != None:
-        if isys["roomWidth"] > 0 and isys["roomDepth"] > 0 and isys["unitHeight"] > 0:
-            roomIndex = (isys["roomWidth"] * isys["roomDepth"]) / ( (isys["roomWidth"] + isys["roomDepth"]) * isys["unitHeight"] )
+    # 計算結果モデル
+    resultJson = {
+        "E_lighting": None,
+        "Es_lighting": None,
+        "BEI_L": None,
+        "lighting":[
+        ]
+    }
+
+    # 室毎（照明系統毎）にループ
+    E_lighting = 0    # 設計一次エネルギー消費量 [GJ]
+    Es_lighting = 0   # 基準一次エネルギー消費量 [GJ]
+
+
+    for isys in inputdata["LightingSystems"]:
+
+        # 建物用途、室用途、室面積の取得
+        (buildingType, roomType, roomArea) = get_roomSpec(isys["floorName"],isys["roomName"],inputdata["Rooms"])
+
+        # 基準一次エネルギー消費量 [MJ]
+        Es_room = RoomStandardValue[buildingType][roomType]["照明"] * roomArea
+        Es_lighting += Es_room  # 出力用に積算
+
+        # 年間照明点灯時間 [時間]
+        opeTime = RoomUsageSchedule[buildingType][roomType]["年間照明点灯時間"]
+
+        ## 室の形状に応じて定められる係数（仕様書4.4）
+        # 室指数
+        if isys["roomIndex"] != None:
+            roomIndex = isys["roomIndex"]
+        elif isys["roomWidth"] != None and isys["roomDepth"] != None and isys["unitHeight"] != None:
+            if isys["roomWidth"] > 0 and isys["roomDepth"] > 0 and isys["unitHeight"] > 0:
+                roomIndex = (isys["roomWidth"] * isys["roomDepth"]) / ( (isys["roomWidth"] + isys["roomDepth"]) * isys["unitHeight"] )
+            else:
+                roomIndex = None
         else:
             roomIndex = None
-    else:
-        roomIndex = None
-    
-    # 補正係数
-    roomIndexCoeff = set_roomIndexCoeff(roomIndex)
-
-    ## 器具毎のループ
-    unitPower = 0
-    for iunit in isys["lightingUnit"]:
         
-        # 室指数による補正
-        rmIx = 1
+        # 補正係数
+        roomIndexCoeff = set_roomIndexCoeff(roomIndex)
 
-        # 制御による効果
-        ctrl = (
-            lightingCtrl["OccupantSensingCTRL"][iunit["OccupantSensingCTRL"]] *
-            lightingCtrl["IlluminanceSensingCTRL"][iunit["IlluminanceSensingCTRL"]] *
-            lightingCtrl["TimeScheduleCTRL"][iunit["TimeScheduleCTRL"]] *
-            lightingCtrl["InitialIlluminationCorrectionCTRL"][iunit["InitialIlluminationCorrectionCTRL"]]
-        )
+        ## 器具毎のループ
+        unitPower = 0
+        for iunit in isys["lightingUnit"]:
+        
+            # 室指数による補正
+            rmIx = 1
 
-        # 照明器具の消費電力（制御込み） [W]
-        unitPower += iunit["RatedPower"] * iunit["Number"] * ctrl
+            # 制御による効果
+            ctrl = (
+                lightingCtrl["OccupantSensingCTRL"][iunit["OccupantSensingCTRL"]] *
+                lightingCtrl["IlluminanceSensingCTRL"][iunit["IlluminanceSensingCTRL"]] *
+                lightingCtrl["TimeScheduleCTRL"][iunit["TimeScheduleCTRL"]] *
+                lightingCtrl["InitialIlluminationCorrectionCTRL"][iunit["InitialIlluminationCorrectionCTRL"]]
+            )
 
-    # 設計一次エネルギー消費量 [MJ]
-    E_room = unitPower * roomIndexCoeff * opeTime * fprime * 10**(-6)
-    E_lighting += E_room  # 出力用に積算
+            # 照明器具の消費電力（制御込み） [W]
+            unitPower += iunit["RatedPower"] * iunit["Number"] * ctrl
 
-
-    # 計算結果を格納
-    resultJson["lighting"].append(
-        {
-            "floorName": isys["floorName"],
-            "roomName": isys["roomName"],
-            "buildingType": buildingType,
-            "roomType": roomType,
-            "roomArea": roomArea,
-            "opelationTime": opeTime,
-            "roomIndex": roomIndex,
-            "roomIndexCoeff": roomIndexCoeff,
-            "unitPower": unitPower,
-            "PrimaryEnergy": E_room,
-            "PrimaryEnergyPerArea": E_room/roomArea,
-            "StandardEnergy": Es_room
-        })
+        # 設計一次エネルギー消費量 [MJ]
+        E_room = unitPower * roomIndexCoeff * opeTime * fprime * 10**(-6)
+        E_lighting += E_room  # 出力用に積算
 
 
-# 建物全体の計算結果
-resultJson["E_lighting"] = E_lighting
-resultJson["Es_lighting"] = Es_lighting
-resultJson["BEI_L"] = E_lighting / Es_lighting
+        if roomArea <= 0:
+            PrimaryEnergyPerArea = None
+        else:
+            PrimaryEnergyPerArea = E_room/roomArea
+
+        if Es_lighting <= 0:
+            BEI_L = None
+        else:
+            BEI_L = E_lighting / Es_lighting
+
+        # 計算結果を格納
+        resultJson["lighting"].append(
+            {
+                "floorName": isys["floorName"],
+                "roomName": isys["roomName"],
+                "buildingType": buildingType,
+                "roomType": roomType,
+                "roomArea": roomArea,
+                "opelationTime": opeTime,
+                "roomIndex": roomIndex,
+                "roomIndexCoeff": roomIndexCoeff,
+                "unitPower": unitPower,
+                "PrimaryEnergy": E_room,
+                "PrimaryEnergyPerArea": PrimaryEnergyPerArea,
+                "StandardEnergy": Es_room
+            })
+
+    # 建物全体の計算結果
+    resultJson["E_lighting"] = E_lighting
+    resultJson["Es_lighting"] = Es_lighting
+    resultJson["BEI_L"] = BEI_L
+
+    return resultJson
 
 
 
+if __name__ == '__main__':
 
-# %%
+    print('----- lighting.py -----')
+    
+    filename = './sample/inputdata.json'
+
+    # テンプレートjsonの読み込み
+    with open(filename, 'r') as f:
+        inputdata = json.load(f)
+
+    resultJson = lighting(inputdata)
+    pp.pprint(resultJson)
