b06d0ed264c5d840505b843355c1ce62841a156c
Masato Miyata <myt.masato@gmail.com>
2020-03-13 23:38:33 +0900

    換気設備の入力シート読み込み作成

--- builelib/modeling/makeJsonFile.py
+++ builelib/modeling/makeJsonFile.py
@@ -20,10 +20,20 @@ with open('./builelib/modeling/webproJsonSchema.json', 'r') as f:
 
 
 # デフォルトを設定する関数
-def set_default(x,default):
-    if x == "":
-        x = default
-    return x
+def set_default(value,default,type):
+    if value == "":
+        out = default
+    else:
+        if type == "str":
+            out = str(value)
+        elif type == "float":
+            out = float(value)
+        elif type == "int":
+            out = int(value)
+        else:
+            out = value
+    return out
+
 
 # %%
 # 様式BLの読み込み
@@ -82,6 +92,85 @@ if "様式RM" in wb.sheet_names():
 
 
 #%% 
+## 機械換気設備
+if "様式V1" in wb.sheet_names():
+    
+    # シートの読み込み
+    sheet_V1 = wb.sheet_by_name("様式V1")
+
+    # 行のループ
+    for i in range(10,sheet_V1.nrows):
+
+        # シートから「行」の読み込み
+        dataV = sheet_V1.row_values(i)
+
+        # 階と室名が空欄でない場合
+        if (dataV[0] != "") and (dataV[1] != ""):
+
+            addDic = {
+                "Floor": str(dataV[0]),
+                "RoomName": str(dataV[1]),
+                "VentilationType": str(dataV[2]),
+                "VentilationUnitRef":[
+                    {
+                        "UnitType": str(dataV[3]),
+                        "Name": str(dataV[4]),
+                        "Info": str(dataV[5])
+                    }
+                ]
+            }
+
+            # リストを追加
+            data["VentilationRoom"].append(addDic)
+
+        # 階と室名が空欄であり、かつ、機器名称に入力がある場合
+        if (dataV[0] == "") and (dataV[1] == "") and (dataV[4] != ""):
+
+            addDic = {
+                "UnitType": str(dataV[3]),
+                "Name": str(dataV[4]),
+                "Info": str(dataV[5])
+            }
+
+            # リストの最後の追加する。
+            data["VentilationRoom"][-1]["VentilationUnitRef"].append(addDic)
+
+
+if "様式V2" in wb.sheet_names():
+    
+    # シートの読み込み
+    sheet_V2 = wb.sheet_by_name("様式V2")
+
+    # 行のループ
+    for i in range(10,sheet_V2.nrows):
+
+        # シートから「行」の読み込み
+        dataV = sheet_V2.row_values(i)
+
+        # 換気機器名称が空欄でない場合
+        if (dataV[0] != ""):
+
+            addDic = {
+                "Name": str(dataV[0]),
+                "Number": set_default(dataV[1], 1, "float"),
+                "FanAirVolume": set_default(dataV[2], None, "float"),
+                "MoterRatedPower": set_default(dataV[3], None, "float"),
+                "PowerConsumption": set_default(dataV[4], None, "float"),
+                "HighEfficiencyMotor": set_default(str(dataV[5]),'無', "str"),
+                "Inverter": set_default(str(dataV[6]),'無', "str"),
+                "AirVolumeControl": set_default(str(dataV[7]),'無', "str"),
+                "VentilationRoomType": set_default(str(dataV[8]),None, "str"),
+                "AC_CoolingCapacity": set_default(dataV[9], None, "float"),
+                "AC_RefEfficiency": set_default(dataV[10], None, "float"),
+                "AC_PumpPower": set_default(dataV[11], None, "float"),
+                "Info": str(dataV[12])
+            }
+
+            # リストを追加
+            data["VentilationUnit"].append(addDic)
+
+
+#%% 
 if "様式L" in wb.sheet_names():
     
     # シートの読み込み
@@ -99,19 +188,19 @@ if "様式L" in wb.sheet_names():
             addDic = {
                 "floorName": str(dataL[0]),
                 "roomName": str(dataL[1]),
-                "roomWidth": set_default(dataL[2],None),
-                "roomDepth": set_default(dataL[3],None),
-                "unitHeight": set_default(dataL[4],None),
-                "roomIndex": set_default(dataL[5],None),
+                "roomWidth": set_default(dataL[2],None, "float"),
+                "roomDepth": set_default(dataL[3],None, "float"),
+                "unitHeight": set_default(dataL[4],None, "float"),
+                "roomIndex": set_default(dataL[5],None, "float"),
                 "lightingUnit":[
                     {
                         "UnitName": str(dataL[6]),
                         "RatedPower": float(dataL[7]),
                         "Number": float(dataL[8]),
-                        "OccupantSensingCTRL": set_default(str(dataL[9]),schema_data["definitions"]["LightingUnit"]["properties"]["OccupantSensingCTRL"]["default"]),
-                        "IlluminanceSensingCTRL": set_default(str(dataL[10]),schema_data["definitions"]["LightingUnit"]["properties"]["IlluminanceSensingCTRL"]["default"]),
-                        "TimeScheduleCTRL": set_default(str(dataL[11]),schema_data["definitions"]["LightingUnit"]["properties"]["TimeScheduleCTRL"]["default"]),
-                        "InitialIlluminationCorrectionCTRL": set_default(str(dataL[12]),schema_data["definitions"]["LightingUnit"]["properties"]["InitialIlluminationCorrectionCTRL"]["default"])
+                        "OccupantSensingCTRL": set_default(str(dataL[9]),schema_data["definitions"]["LightingUnit"]["properties"]["OccupantSensingCTRL"]["default"], "str"),
+                        "IlluminanceSensingCTRL": set_default(str(dataL[10]),schema_data["definitions"]["LightingUnit"]["properties"]["IlluminanceSensingCTRL"]["default"], "str"),
+                        "TimeScheduleCTRL": set_default(str(dataL[11]),schema_data["definitions"]["LightingUnit"]["properties"]["TimeScheduleCTRL"]["default"], "str"),
+                        "InitialIlluminationCorrectionCTRL": set_default(str(dataL[12]),schema_data["definitions"]["LightingUnit"]["properties"]["InitialIlluminationCorrectionCTRL"]["default"], "str")
                     }
                 ]
             }
@@ -119,17 +208,17 @@ if "様式L" in wb.sheet_names():
             # リストを追加
             data["LightingSystems"].append(addDic)
             
-        # 階と室名が空欄である場合
-        if (dataL[0] == "") and (dataL[1] == ""):
+        # 階と室名が空欄であり、かつ、消費電力の入力がある場合
+        if (dataL[0] == "") and (dataL[1] == "") and (dataL[7] != ""):
 
             addDic = {
                 "UnitName": str(dataL[6]),
                 "RatedPower": float(dataL[7]),
                 "Number": float(dataL[8]),
-                "OccupantSensingCTRL": set_default(str(dataL[9]),schema_data["definitions"]["LightingUnit"]["properties"]["OccupantSensingCTRL"]["default"]),
-                "IlluminanceSensingCTRL": set_default(str(dataL[10]),schema_data["definitions"]["LightingUnit"]["properties"]["IlluminanceSensingCTRL"]["default"]),
-                "TimeScheduleCTRL": set_default(str(dataL[11]),schema_data["definitions"]["LightingUnit"]["properties"]["TimeScheduleCTRL"]["default"]),
-                "InitialIlluminationCorrectionCTRL": set_default(str(dataL[12]),schema_data["definitions"]["LightingUnit"]["properties"]["InitialIlluminationCorrectionCTRL"]["default"])
+                "OccupantSensingCTRL": set_default(str(dataL[9]),schema_data["definitions"]["LightingUnit"]["properties"]["OccupantSensingCTRL"]["default"], "str"),
+                "IlluminanceSensingCTRL": set_default(str(dataL[10]),schema_data["definitions"]["LightingUnit"]["properties"]["IlluminanceSensingCTRL"]["default"], "str"),
+                "TimeScheduleCTRL": set_default(str(dataL[11]),schema_data["definitions"]["LightingUnit"]["properties"]["TimeScheduleCTRL"]["default"], "str"),
+                "InitialIlluminationCorrectionCTRL": set_default(str(dataL[12]),schema_data["definitions"]["LightingUnit"]["properties"]["InitialIlluminationCorrectionCTRL"]["default"], "str")
             }
 
             # リストの最後の追加する。
