78982e3d88d581b7403cff72ce64fd709307b88b
MasatoMiyata <myt.masato@gmail.com>
2020-08-18 18:26:01 +0900

    昇降機テスト方法の改良

--- tests/test_elevetor.py
+++ tests/test_elevetor.py
@@ -2,184 +2,210 @@ import pandas as pd
 import csv
 import pprint as pp
 from builelib.elevetor import elevetor
-import jsonschema
+import pytest
 
-# 空欄にデフォルト値を代入する
+### テストファイル名 ###
+# 辞書型 テスト名とファイル名
+
+testcase_dict = {
+    "office": "./tests/elevetor/◇事務所テスト.txt",
+    "office_2units": "./tests/elevetor/◇事務所テスト1室2系.txt"
+}
+
+
 def convert2number(x, default):
+    '''
+    空欄にデフォルト値を代入する
+    '''
     if x == "":
         x = default
     else:
         x = float(x)
     return x
 
-# 計算の実行
-def calculation(filename):
 
-    print(filename) # 確認用
-
-    # テスト用データの読み込み
+def read_testcasefile(filename):
+    '''
+    テストケースファイルを読み込む関数
+    '''
     with open(filename, mode='r', newline='', encoding='shift-jis') as f:
         tsv_reader = csv.reader(f, delimiter=',')
         testdata = [row for row in tsv_reader]
+    return testdata
 
-    # ヘッダーを削除
-    testdata.pop(0)
 
-    for data in testdata:
+def make_inputdata(data):
+    '''
+    インプットデータを作成する関数
+    '''
 
-        print(data) # 確認用
+    if (data[11] == "") and (data[16] == ""):
 
-        if (data[11] == "") and (data[16] == ""):
-
-            # 計算モデルの作成（1室1系統）
-            inputdata = {
-                "Building":{
-                    "Region": "6"
-                },
-                "Rooms": {
-                    "1F_室": {
-                        "floorName": data[1],
-                        "roomName": data[2],
-                        "buildingType": data[3],
-                        "roomType": data[4],
-                        "roomArea": 100,
-                    }
-                },
-                "Elevators": {
-                    "1F_室": {
-                        "Elevator": [
-                            {
-                                "ElevatorName": data[5],
-                                "Number": convert2number(data[6],0),
-                                "LoadLimit": convert2number(data[7],0),
-                                "Velocity": convert2number(data[8],0),
-                                "TransportCapacityFactor": convert2number(data[9],0),
-                                "ControlType": data[10],
-                                "Info": ""
-                            }
-                        ]
-                    }
+        # 計算モデルの作成（1室1系統）
+        inputdata = {
+            "Building":{
+                "Region": "6"
+            },
+            "Rooms": {
+                "1F_室": {
+                    "floorName": data[1],
+                    "roomName": data[2],
+                    "buildingType": data[3],
+                    "roomType": data[4],
+                    "roomArea": 100,
+                }
+            },
+            "Elevators": {
+                "1F_室": {
+                    "Elevator": [
+                        {
+                            "ElevatorName": data[5],
+                            "Number": convert2number(data[6],0),
+                            "LoadLimit": convert2number(data[7],0),
+                            "Velocity": convert2number(data[8],0),
+                            "TransportCapacityFactor": convert2number(data[9],0),
+                            "ControlType": data[10],
+                            "Info": ""
+                        }
+                    ]
                 }
             }
+        }
 
-        elif (data[11] == ""):
+    elif (data[11] == ""):
 
-            # 計算モデルの作成（1室2系統）
-            inputdata = {
-                "Building":{
-                    "Region": "6"
-                },
-                "Rooms": {
-                    "1F_室": {
-                        "floorName": data[1],
-                        "roomName": data[2],
-                        "buildingType": data[3],
-                        "roomType": data[4],
-                        "roomArea": 100,
-                    }
-                },
-                "Elevators": {
-                    "1F_室": {
-                        "Elevator": [
-                            {
-                                "ElevatorName": data[5],
-                                "Number": convert2number(data[6],0),
-                                "LoadLimit": convert2number(data[7],0),
-                                "Velocity": convert2number(data[8],0),
-                                "TransportCapacityFactor": convert2number(data[9],0),
-                                "ControlType": data[10],
-                                "Info": ""
-                            },
-                            {
-                                "ElevatorName": data[15],
-                                "Number": convert2number(data[16],0),
-                                "LoadLimit": convert2number(data[17],0),
-                                "Velocity": convert2number(data[18],0),
-                                "TransportCapacityFactor": convert2number(data[19],0),
-                                "ControlType": data[20],
-                                "Info": ""
-                            }
-                        ]
-                    }
+        # 計算モデルの作成（1室2系統）
+        inputdata = {
+            "Building":{
+                "Region": "6"
+            },
+            "Rooms": {
+                "1F_室": {
+                    "floorName": data[1],
+                    "roomName": data[2],
+                    "buildingType": data[3],
+                    "roomType": data[4],
+                    "roomArea": 100,
+                }
+            },
+            "Elevators": {
+                "1F_室": {
+                    "Elevator": [
+                        {
+                            "ElevatorName": data[5],
+                            "Number": convert2number(data[6],0),
+                            "LoadLimit": convert2number(data[7],0),
+                            "Velocity": convert2number(data[8],0),
+                            "TransportCapacityFactor": convert2number(data[9],0),
+                            "ControlType": data[10],
+                            "Info": ""
+                        },
+                        {
+                            "ElevatorName": data[15],
+                            "Number": convert2number(data[16],0),
+                            "LoadLimit": convert2number(data[17],0),
+                            "Velocity": convert2number(data[18],0),
+                            "TransportCapacityFactor": convert2number(data[19],0),
+                            "ControlType": data[20],
+                            "Info": ""
+                        }
+                    ]
                 }
             }
+        }
 
-        else:
+    else:
 
-            # 計算モデルの作成（2室の場合）
-            inputdata = {
-                "Building":{
-                    "Region": "6"
+        # 計算モデルの作成（2室の場合）
+        inputdata = {
+            "Building":{
+                "Region": "6"
+            },
+            "Rooms": {
+                "1F_室": {
+                    "floorName": data[1],
+                    "roomName": data[2],
+                    "buildingType": data[3],
+                    "roomType": data[4],
+                    "roomArea": 100,
                 },
-                "Rooms": {
-                    "1F_室": {
-                        "floorName": data[1],
-                        "roomName": data[2],
-                        "buildingType": data[3],
-                        "roomType": data[4],
-                        "roomArea": 100,
-                    },
-                    "2F_室": {
-                        "floorName": data[11],
-                        "roomName": data[12],
-                        "buildingType": data[13],
-                        "roomType": data[14],
-                        "roomArea": 100,
-                    },
+                "2F_室": {
+                    "floorName": data[11],
+                    "roomName": data[12],
+                    "buildingType": data[13],
+                    "roomType": data[14],
+                    "roomArea": 100,
                 },
-                "Elevators": {
-                    "1F_室": {
-                        "Elevator": [
-                            {
-                                "ElevatorName": data[5],
-                                "Number": convert2number(data[6],0),
-                                "LoadLimit": convert2number(data[7],0),
-                                "Velocity": convert2number(data[8],0),
-                                "TransportCapacityFactor": convert2number(data[9],0),
-                                "ControlType": data[10],
-                                "Info": ""
-                            }
-                        ]
-                    },
-                    "2F_室": {
-                        "Elevator": [
-                            {
-                                "ElevatorName": data[15],
-                                "Number": convert2number(data[16],0),
-                                "LoadLimit": convert2number(data[17],0),
-                                "Velocity": convert2number(data[18],0),
-                                "TransportCapacityFactor": convert2number(data[19],0),
-                                "ControlType": data[20],
-                                "Info": ""
-                            }
-                        ]
-                    },
-                }
+            },
+            "Elevators": {
+                "1F_室": {
+                    "Elevator": [
+                        {
+                            "ElevatorName": data[5],
+                            "Number": convert2number(data[6],0),
+                            "LoadLimit": convert2number(data[7],0),
+                            "Velocity": convert2number(data[8],0),
+                            "TransportCapacityFactor": convert2number(data[9],0),
+                            "ControlType": data[10],
+                            "Info": ""
+                        }
+                    ]
+                },
+                "2F_室": {
+                    "Elevator": [
+                        {
+                            "ElevatorName": data[15],
+                            "Number": convert2number(data[16],0),
+                            "LoadLimit": convert2number(data[17],0),
+                            "Velocity": convert2number(data[18],0),
+                            "TransportCapacityFactor": convert2number(data[19],0),
+                            "ControlType": data[20],
+                            "Info": ""
+                        }
+                    ]
+                },
             }
+        }
 
-        try:
-            
-            # 計算実行        
-            resultJson = elevetor(inputdata)
-
-            # 期待値
-            resultJson["expectedDesignValue"]   = convert2number(data[21],0)
-            # resultJson["expectedStandardValue"] = convert2number(data[22],0)
-
-            assert abs(resultJson["E_elevetor"] - resultJson["expectedDesignValue"])   < 0.0001
-            # assert abs(resultJson["Es_elevetor"] - resultJson["expectedStandardValue"]) < 0.0001
-
-        except:
-            assert data[21] == "err"
+    return inputdata
 
 
-#### テスト実行 ####
+#### テストケースファイルの読み込み
 
-def test_office_basic():
-    calculation('./tests/elevetor/◇事務所テスト.txt')
+test_to_try = []   # テスト用入力ファイルと期待値のリスト
+testcase_id  = []  # テスト名称のリスト
 
-def test_office_1room2elevetors():
-    calculation('./tests/elevetor/◇事務所テスト1室2系.txt')
+for case_name in testcase_dict:
+
+    # テストファイルの読み込み
+    testfiledata = read_testcasefile(testcase_dict[case_name])
+
+    # ヘッダーの削除
+    testfiledata.pop(0)
+
+    # テストケース（行）に対するループ
+    for testdata in testfiledata:
+
+        # 入力データの作成
+        inputdata = make_inputdata(testdata)
+        # 期待値
+        expectedvalue = convert2number(testdata[21],0)
+
+        # テストケースの集約
+        test_to_try.append( (inputdata, expectedvalue) )
+        # テストケース名
+        testcase_id.append(case_name + testdata[0])
+
+
+# テストの実施
+@pytest.mark.parametrize('inputdata, expectedvalue', test_to_try, ids=testcase_id)
+def test_calc(inputdata, expectedvalue):
+
+    # 計算実行        
+    resultJson = elevetor(inputdata)
+
+    # 比較
+    assert abs(resultJson["E_elevetor"] - expectedvalue)   < 0.0001
 
 
 if __name__ == '__main__':
