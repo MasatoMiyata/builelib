844efc3db408c5ea9f80d2e752d3901a1eb94a63
MasatoMiyata <myt.masato@gmail.com>
2020-12-23 08:53:27 +0900

    （その他）CGS用の出力を追加

--- builelib/other_energy.py
+++ builelib/other_energy.py
@@ -33,7 +33,13 @@ def calc_energy(inputdata, DEBUG = False):
     # 計算結果を格納する変数
     resultJson = {
         "E_other": 0,        # 基準一次エネルギー消費量 [MJ]
-        "E_other_room": {}
+        "E_other_room": {},
+        "for_CGS":{
+            "Edesign_MWh_day": np.zeros(365),
+            "ratio_AreaWeightedSchedule_AC": np.zeros((365,24)),   # 空調
+            "ratio_AreaWeightedSchedule_LT": np.zeros((365,24)),   # 照明
+            "ratio_AreaWeightedSchedule_OA": np.zeros((365,24))    # 機器発熱
+        }
     }
 
     ##----------------------------------------------------------------------------------
@@ -41,6 +47,9 @@ def calc_energy(inputdata, DEBUG = False):
     ##----------------------------------------------------------------------------------
     
     # 機器発熱スケジュールを抽出
+    roomScheduleRoom  = {}
+    roomScheduleLight  = {}
+    roomSchedulePerson  = {}
     roomScheduleOAapp  = {}
     roomHeatGain_OAapp  = {}
 
@@ -49,11 +58,12 @@ def calc_energy(inputdata, DEBUG = False):
         resultJson["E_other_room"][room_name] = {
             "E_other_standard"   : 0,              # 告示の値 [MJ/m2]
             "E_other": 0,                          # その他一次エネルギー消費量 [MJ/年]
+            "E_ratio": 1,
             "roomHeatGain_daily" : np.zeros(365)   # 日別の機器発熱量 [MJ/m2/day]
         }
 
         # 365日×24時間分のスケジュール （365×24の行列を格納した dict型）
-        _, _, _, roomScheduleOAapp[room_name], _ = \
+        roomScheduleRoom[room_name], roomScheduleLight[room_name], roomSchedulePerson[room_name], roomScheduleOAapp[room_name], _ = \
             bc.get_roomUsageSchedule(inputdata["Rooms"][room_name]["buildingType"], inputdata["Rooms"][room_name]["roomType"])
 
         # 発熱量参照値 [W/m2]
@@ -69,6 +79,14 @@ def calc_energy(inputdata, DEBUG = False):
             # その他一次エネルギー消費量原単位（告示の値） [MJ/m2]
             resultJson["E_other_room"][room_name]["E_other_standard"] = \
                 round(np.sum(resultJson["E_other_room"][room_name]["roomHeatGain_daily"]))
+
+            # 年積算値が告示の値と一致するように補正する係数を求める。
+            if np.sum(resultJson["E_other_room"][room_name]["roomHeatGain_daily"]) == 0:
+                resultJson["E_other_room"][room_name]["E_ratio"] = 1
+            else:
+                resultJson["E_other_room"][room_name]["E_ratio"] = \
+                    resultJson["E_other_room"][room_name]["E_other_standard"] / np.sum(resultJson["E_other_room"][room_name]["roomHeatGain_daily"])
+
 
         if DEBUG:
             print(f'室名: {room_name}')
@@ -86,17 +104,76 @@ def calc_energy(inputdata, DEBUG = False):
 
         resultJson["E_other"] += resultJson["E_other_room"][room_name]["E_other"]
 
+        resultJson["for_CGS"]["Edesign_MWh_day"]  += \
+            resultJson["E_other_room"][room_name]["roomHeatGain_daily"] * inputdata["Rooms"][room_name]["roomArea"] * resultJson["E_other_room"][room_name]["E_ratio"] / bc.fprime
+
+    ##----------------------------------------------------------------------------------
+    # コジェネ計算用のスケジュール
+    ##----------------------------------------------------------------------------------
+    AreaWeightedSchedule_AC = np.zeros((365,24))
+    AreaWeightedSchedule_LT = np.zeros((365,24))
+    AreaWeightedSchedule_OA = np.zeros((365,24))
+
+    # 空調スケジュール
+    for room_zone_name in inputdata["AirConditioningZone"]:
+        if room_zone_name in inputdata["Rooms"]:  # ゾーン分けがない場合
+
+            AreaWeightedSchedule_AC += roomScheduleRoom[room_zone_name] * inputdata["Rooms"][room_zone_name]["roomArea"]
+
+        else:
+
+            # 各室のゾーンを検索
+            for room_name in inputdata["Rooms"]:
+                if inputdata["Rooms"][room_name]["zone"] != None:   # ゾーンがあれば
+                    for zone_name  in inputdata["Rooms"][room_name]["zone"]:   # ゾーン名を検索
+                        if room_zone_name == (room_name+"_"+zone_name):
+                            
+                            AreaWeightedSchedule_AC += roomScheduleRoom[room_name] * inputdata["Rooms"][room_name]["zone"][zone_name]["zoneArea"]
+
+    # 照明スケジュール
+    for room_zone_name in inputdata["LightingSystems"]:
+        if room_zone_name in inputdata["Rooms"]:  # ゾーン分けがない場合
+
+            AreaWeightedSchedule_LT += roomScheduleLight[room_zone_name] * inputdata["Rooms"][room_zone_name]["roomArea"]
+
+        else:
+
+            # 各室のゾーンを検索
+            for room_name in inputdata["Rooms"]:
+                if inputdata["Rooms"][room_name]["zone"] != None:   # ゾーンがあれば
+                    for zone_name  in inputdata["Rooms"][room_name]["zone"]:   # ゾーン名を検索
+                        if room_zone_name == (room_name+"_"+zone_name):
+                            
+                            AreaWeightedSchedule_LT += roomScheduleLight[room_name] * inputdata["Rooms"][room_name]["zone"][zone_name]["zoneArea"]               
+
+    # 機器発熱スケジュール                   
+    for room_name in inputdata["Rooms"]:
+        AreaWeightedSchedule_OA += roomScheduleOAapp[room_name] * inputdata["Rooms"][room_name]["roomArea"]
+
+    for dd in range(0,365):
+        resultJson["for_CGS"]["ratio_AreaWeightedSchedule_AC"][dd] = \
+            AreaWeightedSchedule_AC[dd] / np.sum(AreaWeightedSchedule_AC[dd])
+        resultJson["for_CGS"]["ratio_AreaWeightedSchedule_LT"][dd] = \
+            AreaWeightedSchedule_LT[dd] / np.sum(AreaWeightedSchedule_LT[dd])
+        resultJson["for_CGS"]["ratio_AreaWeightedSchedule_OA"][dd] = \
+            AreaWeightedSchedule_OA[dd] / np.sum(AreaWeightedSchedule_OA[dd])
+
+
     if DEBUG:
         print(f'その他一次エネルギー消費量 MJ: {resultJson["E_other"]}')
 
-
+    if DEBUG:
+        with open("resultJson_Other.json",'w') as fw:
+            json.dump(resultJson, fw, indent=4, ensure_ascii=False, cls = MyEncoder)
+            
     return resultJson
 
 
 if __name__ == '__main__':
 
     print('----- other_energy.py -----')
-    filename = './sample/sample01_WEBPRO_inputSheet_for_Ver2.5.json'
+    # filename = './sample/sample01_WEBPRO_inputSheet_for_Ver2.5.json'
+    filename = './sample/CGS_case_office_00.json'
 
     # 入力ファイルの読み込み
     with open(filename, 'r') as f:
