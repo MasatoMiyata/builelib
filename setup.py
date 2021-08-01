bb9ea8b7063de8328da0dead7c466a10aaf8bd53
MasatoMiyata <myt.masato@gmail.com>
2021-08-01 19:18:52 +0900

    Ver.1.1.4に更新

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE') as f:
 
 setup(
     name='builelib',
-    version='1.1.0',
+    version='1.1.4',
     description='builelib: Building Energy-modeling Library',
     author='Masato Miyata',
     author_email='builelib@gmail.com',
