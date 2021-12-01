a33704c4fae5373eb08814ba4cb5b01ffcd77fe3
MasatoMiyata <myt.masato@gmail.com>
2021-12-01 19:16:35 +0900

    二次ポンプ群の計算結果をマトリックス表示する関数を追加

--- builelib/make_figure.py
+++ builelib/make_figure.py
@@ -60,7 +60,7 @@ def hourlyplot(x, lable_text, color_name, title_text):
 
 def histgram_matrix_fan(load_ratio, heat_amount, energy):
     """
-    運転時間のヒストグラム
+    空調機群の計算結果をマトリックス表示する関数
     """
 
     time_cooling   = np.zeros(11)
@@ -129,6 +129,75 @@ def histgram_matrix_fan(load_ratio, heat_amount, energy):
     ax = fig.add_subplot(2,1,2)
     ax.bar(x, time_heating, tick_label=label, align="center")
     ax.grid()
+
+
+
+def histgram_matrix_pump(load_ratio, number_of_operation, energy_consumption):
+    """
+    二次ポンプ群の計算結果をマトリックス表示する関数
+    """
+
+    time   = np.zeros(11)
+    number = np.zeros(11)
+    energy = np.zeros(11)
+
+    for dd in range(365):
+        for hh in range(24):
+
+            # 負荷率帯の区分
+            if load_ratio[dd][hh] > 0:
+                if load_ratio[dd][hh] <= 0.1:
+                    matrix_iL = 0
+                elif  load_ratio[dd][hh] <= 0.2:
+                    matrix_iL = 1
+                elif  load_ratio[dd][hh] <= 0.3:
+                    matrix_iL = 2       
+                elif  load_ratio[dd][hh] <= 0.4:
+                    matrix_iL = 3
+                elif  load_ratio[dd][hh] <= 0.5:
+                    matrix_iL = 4
+                elif  load_ratio[dd][hh] <= 0.6:
+                    matrix_iL = 5
+                elif  load_ratio[dd][hh] <= 0.7:
+                    matrix_iL = 6
+                elif  load_ratio[dd][hh] <= 0.8:
+                    matrix_iL = 7
+                elif  load_ratio[dd][hh] <= 0.9:
+                    matrix_iL = 8
+                elif  load_ratio[dd][hh] <= 1.0:
+                    matrix_iL = 9
+                else:
+                    matrix_iL = 10
+
+                time[matrix_iL] += 1
+                number[matrix_iL] += number_of_operation[dd][hh]
+                energy[matrix_iL] += energy_consumption[dd][hh]
+
+    print("負荷出現時間[h]のマトリックス")
+    print(time)
+
+    print("運転台数のマトリックス")
+    print(number/time)
+
+    print("消費電力[kW]のマトリックス")
+    print(energy/time*1000)
+
+    print("電力消費量[MWh]のマトリックス")
+    print(energy)
+
+    x = [1,2,3,4,5,6,7,8,9,10,11]
+    label = ["0〜0.1","0.1〜0.2","0.2〜0.3","0.3〜0.4","0.4〜0.5","0.5〜0.6",
+        "0.6〜0.7","0.7〜0.8","0.8〜0.9","0.9〜1.0","1.0〜"]
+
+    fig = plt.figure(figsize=(12,6))
+    fig.subplots_adjust(left=0.08, bottom=0.08, right=0.98, top=0.93, wspace=0.27, hspace=0.27)
+
+    ax = fig.add_subplot(2,1,1)
+    ax.bar(x, time, tick_label=label, align="center")
+    ax.grid()
+
+
+
 
 
 
