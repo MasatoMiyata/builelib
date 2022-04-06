3961eb94ed88c77d87f3d3c6d04e4a71faa0f516
MasatoMiyata <myt.masato@gmail.com>
2022-04-06 15:46:16 +0900

    import文を調整

--- builelib/other_energy.py
+++ builelib/other_energy.py
@@ -8,8 +8,6 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from . import commons as bc
-from . import climate
-from . import shading
 
 # データベースファイルの保存場所
 database_directory =  os.path.dirname(os.path.abspath(__file__)) + "/database/"
