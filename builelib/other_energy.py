1059b77e20031a4bad1395adc3a6e604567095e2
MasatoMiyata <myt.masato@gmail.com>
2023-10-28 13:21:31 +0900

    コメントを追加

--- builelib/other_energy.py
+++ builelib/other_energy.py
@@ -1,13 +1,12 @@
 import json
 import numpy as np
-import math
 import os
-import copy
 
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from . import commons as bc
+# from . import commons as bc
+import commons as bc
 
 # データベースファイルの保存場所
 database_directory =  os.path.dirname(os.path.abspath(__file__)) + "/database/"
@@ -65,6 +64,7 @@ def calc_energy(inputdata, DEBUG = False):
         # 365日×24時間分のスケジュール （365×24の行列を格納した dict型）
         if inputdata["Rooms"][room_name]["buildingType"] == "共同住宅":
 
+            # 共同住宅共用部は 0 とする。
             roomScheduleRoom[room_name] = np.zeros((365,24))
             roomScheduleLight[room_name] = np.zeros((365,24))
             roomSchedulePerson[room_name] = np.zeros((365,24))
@@ -73,10 +73,11 @@ def calc_energy(inputdata, DEBUG = False):
 
         else:
 
+            # 標準室使用条件から時刻別の発熱密度比率（0～1。365×24の行列）を作成。SP-6に入力がある場合、任意のカレンダーを反映。
             roomScheduleRoom[room_name], roomScheduleLight[room_name], roomSchedulePerson[room_name], roomScheduleOAapp[room_name], _ = \
                 bc.get_roomUsageSchedule(inputdata["Rooms"][room_name]["buildingType"], inputdata["Rooms"][room_name]["roomType"], input_calendar)
 
-            # 発熱量参照値 [W/m2]
+            # 機器発熱量参照値 [W/m2]の読み込み。SP-9に入力がある場合、任意の値を使用。
             (_, _, roomHeatGain_OAapp[room_name],_) = \
                 bc.get_roomHeatGain(inputdata["Rooms"][room_name]["buildingType"], inputdata["Rooms"][room_name]["roomType"], input_room_usage_condition)
 
@@ -85,7 +86,7 @@ def calc_energy(inputdata, DEBUG = False):
             ##----------------------------------------------------------------------------------
             if "room_schedule" in inputdata["SpecialInputData"]:
 
-                # SP-7に入力されていれば上書きする。
+                # SP-7に入力されていれば、時刻別の発熱密度比率を上書きする。
                 if room_name in inputdata["SpecialInputData"]["room_schedule"]:
 
                     if "室の同時使用率" in inputdata["SpecialInputData"]["room_schedule"][room_name]["schedule"]:
@@ -106,11 +107,11 @@ def calc_energy(inputdata, DEBUG = False):
             resultJson["E_other_room"][room_name]["roomHeatGain_daily"]  = \
                 np.sum(roomScheduleOAapp[room_name],1) * roomHeatGain_OAapp[room_name] / 1000000 * bc.fprime
 
-            # その他一次エネルギー消費量原単位（告示の値） [MJ/m2]
+            # その他一次エネルギー消費量原単位（告示の値） [MJ/m2] の算出（端数処理）
             resultJson["E_other_room"][room_name]["E_other_standard"] = \
                 round(np.sum(resultJson["E_other_room"][room_name]["roomHeatGain_daily"]))
 
-            # 年積算値が告示の値と一致するように補正する係数を求める。
+            # 年積算値（端数調整前）が告示の値と一致するように補正する係数を求める（コジェネ計算時に日別消費電力を年積算値と一致させるために必要）。
             if np.sum(resultJson["E_other_room"][room_name]["roomHeatGain_daily"]) == 0:
                 resultJson["E_other_room"][room_name]["E_ratio"] = 1
             else:
@@ -120,7 +121,8 @@ def calc_energy(inputdata, DEBUG = False):
 
         if DEBUG:
             print(f'室名: {room_name}')
-            print(f'その他一次エネ原単位 MJ/m2: {resultJson["E_other_room"][room_name]["E_other_standard"]}')
+            print(f'その他一次エネ原単位 MJ/m2 : {resultJson["E_other_room"][room_name]["E_other_standard"]}')
+            print(f'補正係数 E_ratio : {resultJson["E_other_room"][room_name]["E_ratio"]}')
 
 
     ##----------------------------------------------------------------------------------
@@ -134,6 +136,7 @@ def calc_energy(inputdata, DEBUG = False):
 
         resultJson["E_other"] += resultJson["E_other_room"][room_name]["E_other"]
 
+        # 日別の電力消費量（告示に掲載の年積算値と一致させるために E_ratio で一律補正する）
         resultJson["for_CGS"]["Edesign_MWh_day"]  += \
             resultJson["E_other_room"][room_name]["roomHeatGain_daily"] * inputdata["Rooms"][room_name]["roomArea"] * resultJson["E_other_room"][room_name]["E_ratio"] / bc.fprime
 
