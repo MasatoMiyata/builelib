e6eaab0dff86b024489e5befd765b63da1bdbe3c
MasatoMiyata <myt.masato@gmail.com>
2025-01-16 15:18:45 +0900

    昇降機のCSV出力を追加

--- builelib/elevator.py
+++ builelib/elevator.py
@@ -2,14 +2,15 @@ import json
 import numpy as np
 import os
 import math
+import pandas as pd
 
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import commons as bc
 
-def calc_energy(inputdata, DEBUG = False):
 
+def calc_energy(inputdata, DEBUG = False, output_dir = ""):
 
     #----------------------------------------------------------------------------------
     # 計算結果を格納する変数
@@ -172,13 +173,34 @@ def calc_energy(inputdata, DEBUG = False):
     # 日積算値
     resultJson["for_CGS"]["Edesign_MWh_day"] = np.sum(Edesign_MWh_hour,1)
 
+
+    ##----------------------------------------------------------------------------------
+    # CSV出力
+    ##----------------------------------------------------------------------------------
+    if output_dir != "":
+        output_dir = output_dir + "_"
+
+    df_daily_energy = pd.DataFrame({
+        '一次エネルギー消費量（昇降機）[GJ]'  : resultJson["for_CGS"]["Edesign_MWh_day"] *  (bc.fprime) /1000,
+        '電力消費量（昇降機）[MWh]'  : resultJson["for_CGS"]["Edesign_MWh_day"],
+    }, index=bc.date_1year)
+
+    df_daily_energy.to_csv(output_dir + 'result_EV_Energy_daily.csv', index_label="日時", encoding='CP932')
+
+
     return resultJson
 
 
 if __name__ == '__main__':
 
+    # 現在のスクリプトファイルのディレクトリを取得
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # 1つ上の階層のディレクトリパスを取得
+    parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
+
     print('----- elevator.py -----')
-    filename = './sample/Builelib_sample_SP7-1.json'
+    filename = parent_dir + '/sample/sample01_WEBPRO_inputSheet_for_Ver3.6.json'
 
     # 入力ファイルの読み込み
     with open(filename, 'r', encoding='utf-8') as f:
