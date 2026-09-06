e2483844a3e8d5b1a92c6c39d424a160b0e2060c
MasatoMiyata <myt.masato@gmail.com>
2026-09-06 10:46:57 +0900

    Ver.2.3.1公開準備

--- src/builelib/__init__.py
+++ src/builelib/__init__.py
@@ -1,6 +1,6 @@
 
 __copyright__    = 'Copyright (C) 2020 Masato Miyata'
-__version__      = '2.3.0'
+__version__      = '2.3.1'
 __license__      = 'MIT License'
 __author__       = 'Masato Miyata'
 __author_email__ = 'builelib@gmail.com'
